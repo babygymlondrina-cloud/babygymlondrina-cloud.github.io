c834f93819967edd24abf9cd5e72a82ed8938618
BGL CI <ci@babygymlondrina.com.br>
2026-07-23 12:35:00 +0000

    hml: deploy a partir de bgl@c30d10c

--- hml/config.js
+++ hml/config.js
@@ -8,8 +8,8 @@
 const BGL_CONFIG = {
   // Ambiente: 'prod' em produção, 'hml' injetado pelo CI no deploy de homologação
   env: 'hml',
-  deployVersion: '41e58e7', // substituído pelo CI: hash curto do commit
-  deployAt: '2026-07-23T12:34:47Z', // substituído pelo CI: ISO timestamp do deploy
+  deployVersion: 'c30d10c', // substituído pelo CI: hash curto do commit
+  deployAt: '2026-07-23T12:35:00Z', // substituído pelo CI: ISO timestamp do deploy
 
   // Google Analytics 4
   gaId: 'G-QPB21Y8RKP',
