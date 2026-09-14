9a03aef24c9939569353ed41969f938d48926969
BGL CI <ci@babygymlondrina.com.br>
2026-09-14 16:51:00 +0000

    prod: promove hml → prod v3.9 (hml@42a0866)

--- config.js
+++ config.js
@@ -8,8 +8,8 @@
 const BGL_CONFIG = {
   // Ambiente: 'prod' em produção, 'hml' injetado pelo CI no deploy de homologação
   env: 'prod',
-  deployVersion: '31ffb2a', // substituído pelo CI: hash curto do commit
-  deployAt: '2026-09-14T15:47:23Z', // substituído pelo CI: ISO timestamp do deploy
+  deployVersion: '6d5a1e1', // substituído pelo CI: hash curto do commit
+  deployAt: '2026-09-14T16:49:54Z', // substituído pelo CI: ISO timestamp do deploy
 
   // Google Analytics 4
   gaId: 'G-QPB21Y8RKP',
